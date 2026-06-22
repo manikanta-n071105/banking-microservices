c1d4e64dc9ac271fea126e54b31369daa78e83b5
Manikanta <nmanikanta792143@gmail.com>
2026-06-22 10:29:03 +0530

    Fixed datasource URL

--- account-service/src/main/java/com/bank/accountservice/feign/CustomerFeignClient.java
+++ account-service/src/main/java/com/bank/accountservice/feign/CustomerFeignClient.java
@@ -5,10 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-        name = "customer-service",
-        url = "${customer.service.url}"
-)
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerFeignClient {
 
     @GetMapping("/customers/{id}")
